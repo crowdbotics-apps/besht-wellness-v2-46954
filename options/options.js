f100e737a2637827fcc17e991cda29515434c5d2
Crowdbotics <team@crowdbotics.com>
2024-03-12 05:46:25 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "besht-wellness-v2-46954",
+  url: "https://besht-wellness-v2-46954.botics.co",
+  api: "https://besht-wellness-v2-46954.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
